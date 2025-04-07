0730b2e736f6a77f9ddaabc12c7fecb94068a0fc
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-07 22:12:42 +0300

    update: window setup

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # intro to PyQt6
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
 
 # define the Qt window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # set title and window size
+        self.setWindowTitle("Weather app")
+        self.setGeometry(100, 100, 800, 600)
+        # window icon
+        self.setWindowIcon(QIcon("cloud.png"))
 
 def main():
     app = QApplication(sys.argv)
