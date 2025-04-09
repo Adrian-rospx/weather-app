4b6e837f5fe0acf7b77e85d8e691a651622a14d0
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-09 23:48:14 +0300

    learn: CSS styling for Qt

--- main.py
+++ main.py
@@ -1,38 +1,68 @@
 # intro to PyQt6
 import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QLabel,
-                             QLineEdit, QPushButton)
-from PyQt6.QtGui import QIcon, QFont, QPixmap
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QApplication, QMainWindow,
+                             QPushButton, QWidget, QHBoxLayout)
+from PyQt6.QtGui import QIcon
 
 # define the Qt window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Weather app")
-        self.setGeometry(200, 200, 800, 600)
+        #self.setGeometry(200, 200, 800, 600)
         self.setWindowIcon(QIcon("cloud.png"))
         
-        self.line_edit = QLineEdit(self)
-        self.button = QPushButton("Submit", self)
+        self.button1 = QPushButton("#1")
+        self.button2 = QPushButton("#2")
+        self.button3 = QPushButton("#3")
 
         self.initUI()
 
     # function for defining the UI
     def initUI(self):
-        self.line_edit.setGeometry(10,10,400,70)
-        self.button.setGeometry(10, 90, 100, 30)
-        self.line_edit.setStyleSheet("font-size: 40px;" \
-                                     "font-family: Arial;")
-        self.button.setStyleSheet("font-size: 15px;" \
-                                  "font-family: Times new roman;")
-        self.line_edit.setPlaceholderText("Enter your name")
-        
-        self.button.clicked.connect(self.submit)
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
 
-    def submit(self):
-        text = self.line_edit.text()
-        print(f"Hello, {text}!")
+        hbox = QHBoxLayout()
+
+        hbox.addWidget(self.button1)
+        hbox.addWidget(self.button2)
+        hbox.addWidget(self.button3)
+
+        centralWidget.setLayout(hbox)
+
+        self.button1.setObjectName("button1")
+        self.button2.setObjectName("button2")
+        self.button3.setObjectName("button3")
+
+        self.setStyleSheet("""
+            QPushButton{
+                font-size: 40px;
+                font-family: Montserrat;
+                padding: 15px 40px;
+                margin: 25px;
+                border: 3px solid white;
+                border-radius: 15px;
+            }
+            QPushButton#button1{
+                background-color: hsl(20, 82%, 59%);
+            }
+            QPushButton#button2{
+                background-color: hsl(110, 82%, 59%);
+            }
+            QPushButton#button3{
+                background-color: hsl(190, 82%, 59%);
+            }
+            QPushButton#button1:hover{
+                background-color: hsl(20, 82%, 79%);
+            }
+            QPushButton#button2:hover{
+                background-color: hsl(110, 82%, 79%);
+            }
+            QPushButton#button3:hover{
+                background-color: hsl(190, 82%, 79%);
+            }
+        """)
 
 def main():
     app = QApplication(sys.argv)
