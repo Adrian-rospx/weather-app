61e5108ed619f53d05d005d35f3e5fec73467cfd
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-10 23:23:28 +0300

    feat: beginning the application, basic items

--- main.py
+++ main.py
@@ -1,77 +1,36 @@
 # intro to PyQt6
 import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow,
-                             QPushButton, QWidget, QHBoxLayout)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
+                             QLabel, QPushButton, QLineEdit,
+                             QHBoxLayout)
 from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
 
 # define the Qt window
-class MainWindow(QMainWindow):
+class weather_app(QWidget):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Weather app")
-        #self.setGeometry(200, 200, 800, 600)
         self.setWindowIcon(QIcon("cloud.png"))
         
-        self.button1 = QPushButton("#1")
-        self.button2 = QPushButton("#2")
-        self.button3 = QPushButton("#3")
+        self.city_label = QLabel("Enter your city: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get weather", self)
+        self.temperature_label = QLabel("20°C", self)
+        self.description = QLabel("warm and clear", self)
+        
 
         self.initUI()
 
     # function for defining the UI
     def initUI(self):
-        centralWidget = QWidget(self)
-        self.setCentralWidget(centralWidget)
-
-        hbox = QHBoxLayout()
-
-        hbox.addWidget(self.button1)
-        hbox.addWidget(self.button2)
-        hbox.addWidget(self.button3)
-
-        centralWidget.setLayout(hbox)
-
-        self.button1.setObjectName("button1")
-        self.button2.setObjectName("button2")
-        self.button3.setObjectName("button3")
-
-        self.setStyleSheet("""
-            QPushButton{
-                font-size: 40px;
-                font-family: Montserrat;
-                padding: 15px 40px;
-                margin: 25px;
-                border: 3px solid white;
-                border-radius: 15px;
-            }
-            QPushButton#button1{
-                background-color: hsl(20, 82%, 59%);
-            }
-            QPushButton#button2{
-                background-color: hsl(110, 82%, 59%);
-            }
-            QPushButton#button3{
-                background-color: hsl(190, 82%, 59%);
-            }
-            QPushButton#button1:hover{
-                background-color: hsl(20, 82%, 79%);
-            }
-            QPushButton#button2:hover{
-                background-color: hsl(110, 82%, 79%);
-            }
-            QPushButton#button3:hover{
-                background-color: hsl(190, 82%, 79%);
-            }
-        """)
+        ...
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()
-    
-    # display the window on the screen
+    window = weather_app()
     window.show()
-
-    # method to show until the app is closed
     sys.exit(app.exec())
 
 if __name__ == "__main__":
