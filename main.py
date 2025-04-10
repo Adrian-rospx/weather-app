8f6799172a0a2e33a22c2980b9a645b3e9b830a7
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-10 23:38:50 +0300

    update: basic feature ideas into project

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
                              QLabel, QPushButton, QLineEdit,
-                             QHBoxLayout)
+                             QVBoxLayout)
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 
@@ -10,22 +10,35 @@ from PyQt6.QtCore import Qt
 class weather_app(QWidget):
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("Weather app")
-        self.setWindowIcon(QIcon("cloud.png"))
-        
-        self.city_label = QLabel("Enter your city: ", self)
+        self.city_label = QLabel("Enter the name of your city: ", self)
         self.city_input = QLineEdit(self)
         self.get_weather_button = QPushButton("Get weather", self)
         self.temperature_label = QLabel("20°C", self)
         self.description = QLabel("warm and clear", self)
-        
 
         self.initUI()
 
     # function for defining the UI
     def initUI(self):
-        ...
+        self.setWindowTitle("Weather app")
+        self.setWindowIcon(QIcon("cloud.png"))
+        
+        # use vbox to organize all labels
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.description)
+        self.setLayout(vbox)
+
+        self.city_label.setObjectName("city_label")
+        self.city_label.setObjectName("city_input")
+        self.city_label.setObjectName("get_weather_button")
+        self.city_label.setObjectName("temperature_label")
+        self.city_label.setObjectName("description_label")
+
+        
 
 def main():
     app = QApplication(sys.argv)
